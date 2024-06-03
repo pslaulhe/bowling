336856b8b04bb56b32873b5adb313829d09caeaa
pslaulhe <pslaulhe@gmail.com>
2024-06-03 09:08:50 +0200

    Spares

--- Game.test.ts
+++ Game.test.ts
@@ -14,4 +14,18 @@ describe('Game', () => {
 
         expect(game.score()).toBe(score);
     });
+
+
+    it.each(
+        [
+            [[5, 5, 4, 5, 0, 0, 6, 4, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 39],
+            [[0, 2, 7, 3, 7, 3, 4, 5, 6, 4, 4, 4, 0, 0, 0, 0, 0, 0, 5, 4], 73],
+            [[9, 1, 9, 0, 6, 4, 0, 0, 0, 0, 5, 1, 3, 7, 1, 0, 0, 0, 0, 9], 65],
+        ]
+    )('spares %p: %p', (rolls, score)=>{
+        const game = new Game();
+        rolls.forEach(roll => game.roll(roll));
+
+        expect(game.score()).toBe(score);
+    });
 });
--- Game.ts
+++ Game.ts
@@ -1,7 +1,19 @@
 export class Game{
     totalScore = 0;
+    rolls: number[] = [];
+
     roll(pins: number): void {
-        this.totalScore += pins;
+        console.log(pins);
+
+        if (this.rolls.length >= 2 && this.rolls.length % 2 == 0 && this.rolls[this.rolls.length - 2] + this.rolls[this.rolls.length - 1] == 10) {
+            console.log('spare');
+            this.totalScore += pins * 2;
+        }
+        else {
+            this.totalScore += pins;
+        }
+
+        this.rolls.push(pins);
     }
 
     score(): number {
