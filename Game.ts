20accd4ed802c90f3bc7bcef44a22cb02c57af2c
pslaulhe <pslaulhe@gmail.com>
2024-06-04 11:00:01 +0200

    Contract (parallel change)

--- Game.ts
+++ Game.ts
@@ -1,46 +1,6 @@
 export class Game {
-    totalScore = 0;
-    rolls: number[] = [];
-    midFrame: boolean = false;
-    framesCount = 0;
-    extraRollNumber = 0;
-
-    rollOld(pins: number): void {
-        let currentRollScore = this.framesCount < 10 ? pins : 0;
-        if (this.framesCount == 10) {
-            this.extraRollNumber += 1;
-        }
-
-        if (this.rolls.length > 0 && this.rolls[this.rolls.length - 1] == 10 && this.extraRollNumber < 2) {
-            currentRollScore += pins;
-        }
-        if (this.rolls.length > 1 && this.rolls[this.rolls.length - 2] == 10){
-            currentRollScore += pins;
-        }
-
-        if (this.rolls.length > 1 && !this.midFrame &&
-            (this.rolls[this.rolls.length - 1] < 10 && this.rolls[this.rolls.length - 2] < 10 && this.rolls[this.rolls.length - 2] + this.rolls[this.rolls.length - 1] == 10)) {
-            console.log('spare');
-            currentRollScore += pins;
-        }
-
-        console.log('roll score ' + currentRollScore);
-        if (!this.midFrame && pins < 10) {
-            this.midFrame = true;
-        }
-        else if (this.midFrame) {
-            this.midFrame = false;
-            this.framesCount += 1;
-        }
-        else if (this.framesCount < 10) {
-            this.framesCount += 1;
-        }
-
-        this.totalScore += currentRollScore;
-        this.rolls.push(pins);
-    }
-
     frames: Frame[] = [new Frame()];
+
     roll(pins: number): void {
         let currentFrame = this.getCurrentFrame();
         for (let frame of this.frames) {
@@ -56,10 +16,6 @@ export class Game {
             this.frames.push(currentFrame);
         }
         return currentFrame;
-    }
-
-    scoreOld(): number {
-        return this.totalScore;
     }
 
     score(){
